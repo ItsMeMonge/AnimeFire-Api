c9926aa515800c2a4fa17573f16ac96f89df73dc
ItsMeMonge <dark.code504@gmail.com>
2022-01-16 22:08:52 -0300

    teste na api melhoria nas categorias (joao brabo)

--- api.js
+++ api.js
@@ -1,15 +1,67 @@
-const readlineSync = require('readline-sync');
-const puppeteer = require('puppeteer');
-// ===========================================================
-const animeSearch = readlineSync.question('Nome Do Anime: ');
-str = animeSearch.replace(/\s+/g, '-');
 
-const animeName = 'https://animefire.net/animes/'+`${str}`+'-todos-os-episodios';
 
-(async () => {
+async function GetEpisodes () {
+  const readlineSync = require('readline-sync');
+  const puppeteer = require('puppeteer');
+  // ===========================================================
+  const animeSearch = readlineSync.question('Nome Do Anime: ');
+  str = animeSearch.replace(/\s+/g, '-');
+  
+  const epsodio = readlineSync.question('Numero Do Epsodio: ')
+  const animeName = `https://animefire.net/animes/${str}/${epsodio}`;
+  
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(`${animeName}`);
+
+  // Pega todas informações sobre o anime
+  const anime = await page.evaluate(() => {
+    return {
+        teste: document.querySelector("#my-video").dataset.videoSrc.substring(21),
+    };
+  });
+  // agora eu tenho que acessar a pagina do json e pegar o link do epsodio
+
+  //console.log('anime:', anime);
+
+  await browser.close();
+
+// =================================================================
+
+    const arowser = await puppeteer.launch();
+    const aage = await arowser.newPage();
+    await aage.goto(`https://animefire.net/${anime.teste}`);
+
+    // Pega todas informações sobre o anime
+    const link_json = await aage.evaluate(() => {
+      return {
+          episodio: document.querySelector("body > pre").innerText,
+      };
+    });
+    // agora eu tenho que acessar a pagina do json e pegar o link do epsodio
+  
+    console.log('epsodio:', link_json);
+  
+    await arowser.close();
+};
+
+async function GetInfoAnime (){
+
+  const readlineSync = require('readline-sync');
+  const puppeteer = require('puppeteer');
+  // ===========================================================
+  const animeSearch = readlineSync.question('Nome Do Anime: ');
+  str = animeSearch.replace(/\s+/g, '-');
+  
+  const animeName = 'https://animefire.net/animes/'+`${str}`+'-todos-os-episodios';
+  
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`${animeName}`);
+
+
 
   // Pega todas informações sobre o anime
   const anime = await page.evaluate(() => {
@@ -19,7 +71,7 @@ const animeName = 'https://animefire.net/animes/'+`${str}`+'-todos-os-episodios'
       
       Imagem: document.querySelector("#body-content > div.container.mt-5 > div > div.col-lg-9.text-white.divDivAnimeInfo > div.divMainNomeAnime > div.row.mx-2.mt-md-2.divImgAnimePageInfo > div > img").dataset.src,
 
-      Categorias: document.querySelector("#body-content > div.container.mt-5 > div > div.col-lg-9.text-white.divDivAnimeInfo > div.divMainNomeAnime > div.main_div_anime_info > div > div.divAnimePageInfo.ml-2.ml-sm-0.d-none > div:nth-child(1)").innerText,
+      Categorias: [...document.querySelectorAll(".spanGeneros")].map(e => e.innerText).join(",").toLowerCase(),
 
       Ano: document.querySelector("#body-content > div.container.mt-5 > div > div.col-lg-9.text-white.divDivAnimeInfo > div.divMainNomeAnime > div.main_div_anime_info > div > div.divAnimePageInfo.ml-2.ml-sm-0.d-none > div:nth-child(15) > span").innerText,
 
@@ -35,7 +87,7 @@ const animeName = 'https://animefire.net/animes/'+`${str}`+'-todos-os-episodios'
   console.log('anime:', anime);
 
   await browser.close();
-})();
+};
 
-
+GetInfoAnime()
 
